0ac6e3a2da23fc078155a51ee81426995ae7ae16
vacexpfleger <c4n4l123@gmail.com>
2020-10-20 16:23:15 +0200

    finalni verze kostky

--- js/script.js
+++ js/script.js
@@ -1,25 +1,37 @@
 const kostka = document.getElementById("kostka");
 const statistika = document.getElementById("statistika");
-const button = document.getElementById("button");
+const buttonPlay = document.getElementById("buttonPlay");
+const buttonReset = document.getElementById("buttonReset");
+const last = document.getElementById("last");
+const counter = document.getElementById("counter");
+const summary = document.getElementById("summary");
+const avg = document.getElementById("avg");
+const maxi= document.getElementById("max");
+const mini = document.getElementById("min");
+let shake = document.getElementById("shake");
+let roll = document.getElementById("roll");
 let hod = 1;
 let hody = [];
 let timer = false;
 
 function animace() {
   hod = Math.ceil(Math.random() * 6);
-  kostka.src = "img/kostka" + hod + ".png";
+  kostka.src = "img/dice" + hod + ".png";
 }
 
-button.addEventListener("click", function(){
+buttonPlay.addEventListener("click", function(){
   if(!timer){
     timer = setInterval(animace,50);
-    button.innerText = "Stop";
+    shake.play();
+    buttonPlay.innerText = "Stop";
   } else {
+    shake.pause();
+    roll.play();
     clearInterval(timer);
     timer = false;
     hody.push(hod);
     vypisStatistiky();
-    button.innerText = "Hrej";
+    buttonPlay.innerText = "Hrej";
   }
 })
 
@@ -48,10 +60,26 @@ function min() {
 }
 
 function vypisStatistiky() {
-  statistika.innerHTML = `<p>Poslední hod: ${hod}</p>`;
-  statistika.innerHTML += `<p>Počet hodů: ${hody.length}</p>`;
-  statistika.innerHTML += `<p>Součet: ${suma()}</p>`;
-  statistika.innerHTML += `<p>Průměr: ${(suma() / hody.length).toFixed(2)}</p>`;
-  statistika.innerHTML += `<p>Maximum: ${max()}</p>`;
-  statistika.innerHTML += `<p>Minimum: ${min()}</p>`;
+  last.innerText = `Poslední hod: ${hod}`;
+  counter.innerText = `Počet hodů: ${hody.length}`;
+  summary.innerText = `Součet: ${suma()}`;
+  avg.innerText = `Průměr: ${(suma() / hody.length).toFixed(2)}`;
+  maxi.innerText = `Maximum: ${max()}`;
+  mini.innerText = `Minimum: ${min()}`;
 }
+
+buttonReset.addEventListener("click", function(){
+  last.innerText = `Poslední hod: 0`;
+  counter.innerText = `Počet hodů: 0`;
+  summary.innerText = `Součet: 0`;
+  avg.innerText = `Průměr: 0`;
+  maxi.innerText = `Maximum: 0`;
+  mini.innerText = `Minimum: 0`;
+
+  hod = 1;
+  hody = [];
+  timer = false;
+  sum = 0;
+  maximum = 0;
+  minimum = 0;
+})
